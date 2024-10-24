c17f89d989de97fe3798943299b93bd37a68cac6
sameer3841 <81244405+sameer3841@users.noreply.github.com>
2024-10-24 19:30:15 -0400

    added rectangle, xPos slider, radius slider, and color picker

--- app/src/main/java/com/saunakpatel/customview/MainActivity.kt
+++ app/src/main/java/com/saunakpatel/customview/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.saunakpatel.customview
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.CheckBox
-import androidx.activity.enableEdgeToEdge
+import android.widget.RadioGroup
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var myView1: MyView1;
-    lateinit var checker: CheckBox;
+    private lateinit var myView1: MyView1;
+    private lateinit var checker: CheckBox;
+    private lateinit var pos: SeekBar;
+    private lateinit var rad: SeekBar;
+    private lateinit var colorGroup: RadioGroup;
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,13 +24,61 @@ class MainActivity : AppCompatActivity() {
 
         myView1 = findViewById(R.id.myView1)
         checker = findViewById(R.id.checker)
+        pos = findViewById(R.id.pos)
+        rad = findViewById(R.id.rad)
+        colorGroup = findViewById(R.id.colorGroup)
 
         myView1.drawCircle = false
 
         checker.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) myView1.drawCircle = true
-            else myView1.drawCircle = false
+            myView1.drawCircle = isChecked
             myView1.invalidate()
         }
+
+        colorGroup.setOnCheckedChangeListener { _, _ -> update() }
+
+
+        pos.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val maxProgress = seekBar?.max ?: 100
+                val screenWidth = resources.displayMetrics.widthPixels
+                val circleRadius = myView1.circleRad
+
+                var newX = (progress.toFloat() / maxProgress) * (screenWidth - 2 * circleRadius)
+
+                if (progress == pos.min || newX < circleRadius) {
+                    newX = circleRadius
+                } else if (newX + circleRadius > screenWidth - circleRadius || progress == pos.max) {
+                    newX = screenWidth - circleRadius
+                }
+
+                myView1.circleX = newX
+                myView1.invalidate()
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        rad.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val newRadius = 10f + (progress.toFloat() / seekBar?.max!!) * 190f
+                myView1.circleRad = newRadius
+                myView1.circleX = myView1.circleRad
+                myView1.invalidate()
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+    }
+
+    fun update(){
+        when (colorGroup.checkedRadioButtonId) {
+            R.id.red -> myView1.color = Color.RED
+            R.id.blue -> myView1.color = Color.BLUE
+            R.id.green -> myView1.color = Color.rgb(0,255,0)
+        }
+
+        myView1.invalidate()
     }
 }
--- app/src/main/java/com/saunakpatel/customview/MyView1.kt
+++ app/src/main/java/com/saunakpatel/customview/MyView1.kt
@@ -1,5 +1,6 @@
 package com.saunakpatel.customview
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
@@ -12,13 +13,28 @@ import android.view.View
 class MyView1(context: Context?, attrs: AttributeSet?): View(context, attrs) {
     var drawCircle: Boolean = false
     private var paint: Paint
+
+    var circleRad: Float = 10f
+    var circleX: Float = circleRad
+    private var color: Int = Color.RED;
+
+
+
+
     init {
         paint = Paint()
+
     }
 
+    @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: Canvas){
-        paint.setColor(Color.RED)
-        if (drawCircle) canvas.drawCircle(200f, 200f, 80f, paint)
+        paint.setColor(color)
+        val paintRect = Paint()
+        paintRect.setColor(Color.BLUE)
+        if (drawCircle) canvas.drawCircle(circleX, 200f, circleRad, paint)
         else canvas.drawColor(Color.TRANSPARENT)
+
+        canvas.drawRect(10f, 600f, 1070f, 800f, paintRect)
+
     }
 }
